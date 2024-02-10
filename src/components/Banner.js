8a37ae62d06b2ad3fb7d3fcfbba74843a2b12145
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-02-10 05:53:47 +0500

    adding Banner file

---
+++ src/components/Banner.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { Col,Row, Container } from "react-bootstrap";
+import { FaArrowCircleRight } from "react-icons/fa";
+export const Banner = () => {
+    
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const rotating = ['Web dev','UI/UX','Web Design']
+    const [text,setText] = useState('');
+    const period = 3000;
+    const [delta,setDelta] = useState(300 - Math.random()*100);
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            ticker();
+        }, delta);
+    },)
+    return(
+        <section className="banner">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7} >
+                        <span className="tagline">Welcome to my portfolio</span>
+                        <h1>{`Hi I'm AbdelKerim`}<span className="wrap">I'm a web developer</span></h1>
+                        <p>Lorem Ipsum dolor Sit Amet </p>
+                        <button onClick={console.log('Lets play')}>Let's connect <FaArrowCircleRight size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src=''></img>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home')
+    const [scrolled, setScrolled] = useState(false)
+
+    useEffect (() => {
+        const onScroll = () => {
+            if(window.scrollY>50) {
+                setScrolled(true);
+            }
+            else {
+                setScrolled(false);
+            }
+        }
+        window.addEventListener('scroll', onScroll);
+        return(window.removeEventListener('scroll',onScroll));
+    },[])
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+    return (
+
+      <>
+        <div className="nav-full">
+            <h1>Logo</h1>
+            <span className="nav-links">
+              <a href="home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</a>
+              <a href="skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</a>
+              <a href="projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</a>
+            </span>
+            <span className="nav-social">
+              <a href="fb"><FaFacebookF /></a>
+              <a href="tw"><FaTwitter /></a>
+              <a href="yt"><FaInstagram /></a>
+              
+            </span>
+            <button onClick={console.log('Hi')}>Let's connect</button>
+        </div>
+      </>
+    )
+  }
+  
+  export default NavBar;
+  
+  
+  
