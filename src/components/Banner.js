3c11207b32f74a8098fe6f1436605e0767250bc4
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-02-24 08:16:34 +0500

    updating the banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,9 +11,32 @@ export const Banner = () => {
     const [delta,setDelta] = useState(300 - Math.random()*100);
     useEffect(() => {
         let ticker = setInterval(() => {
-           
+           ticking();
         }, delta);
-    },)
+
+        return() => {clearInterval(ticker)}
+    },{text})
+
+    const ticking = () => {
+        let i = loopNum % rotating.length;
+        let fullText = rotating(i);
+        let updatedText = isDeleting ? fullText.substring(0,text.length - 1) : fullText.substring(0,text.length + 1);
+
+        setText = (updatedText);
+
+        if(isDeleting) {
+            setDelta(prevDelta => prevDelta/2)
+        }
+        else if(!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        }
+        else if (isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1)
+            setDelta(500);
+        }
+    }
     return(
         <section className="banner">
             <Container>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,20 @@ import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home')
-    const [scrolled, setScrolled] = useState(false)
+    // const [scrolled, setScrolled] = useState(false)
 
-    useEffect (() => {
-        const onScroll = () => {
-            if(window.scrollY>50) {
-                setScrolled(true);
-            }
-            else {
-                setScrolled(false);
-            }
-        }
-        window.addEventListener('scroll', onScroll);
-        return(window.removeEventListener('scroll',onScroll));
-    },[])
+    // useEffect (() => {
+    //     const onScroll = () => {
+    //         if(window.scrollY>50) {
+    //             setScrolled(true);
+    //         }
+    //         else {
+    //             setScrolled(false);
+    //         }
+    //     }
+    //     window.addEventListener('scroll', onScroll);
+    //     return(window.removeEventListener('scroll',onScroll));
+    // },[])
 
     const onUpdateActiveLink = (value) => {
         setActiveLink(value);
